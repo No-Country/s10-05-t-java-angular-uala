a60298c685203a4aaaca2ca680ee5a12d809aa9d
Marcos Paz Goncheff <69874820+marcosep192000@users.noreply.github.com>
2023-08-21 00:26:39 -0300

    :zap: refactor: security

--- backend/uala/src/main/java/com/noCountry/uala/security/controller/AuthController.java
+++ backend/uala/src/main/java/com/noCountry/uala/security/controller/AuthController.java
@@ -7,10 +7,12 @@ import com.noCountry.uala.security.entity.Rol;
 import com.noCountry.uala.security.entity.Usuario;
 import com.noCountry.uala.security.enums.RolNombre;
 import com.noCountry.uala.security.jwt.JwtProvider;
+import com.noCountry.uala.security.repository.UsuarioRepository;
 import com.noCountry.uala.security.service.RolService;
 import com.noCountry.uala.security.service.UsuarioService;
 import com.noCountry.uala.security.util.Mensaje;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -29,53 +31,21 @@ import java.util.Set;
 @RestController
 @RequestMapping("/v1/api/auth")
 public class AuthController {
+
 	@Autowired
-	PasswordEncoder passwordEncoder;
-	@Autowired
-	AuthenticationManager authenticationManager;
-	@Autowired
-	UsuarioService usuarioService;
-	@Autowired
-	RolService rolService;
-	@Autowired
-	JwtProvider jwtProvider;
+	private UsuarioService usuarioService;
+
 	//Espera un json y lo convierte a tipo clase NuevoUsuario
 	@PostMapping("/add")
-	public ResponseEntity<?> nuevoUsuario(@Valid @RequestBody NuevoUsuario nuevoUsuario,
-										  BindingResult bindingResult) {
-		if(bindingResult.hasErrors()){
-			return new ResponseEntity<>(new Mensaje("Campos mal o email invalido"), HttpStatus.BAD_REQUEST);
-		}
-		if(usuarioService.existsByUsuario(nuevoUsuario.getNombreUsuario() )){
-			return new ResponseEntity<>(new Mensaje("Ese nombre ya existe"), HttpStatus.BAD_REQUEST);
-		}
-		if(usuarioService.existsByEmail(nuevoUsuario.getEmail())){
-			return new ResponseEntity<>(new Mensaje("Ese email ya existe"), HttpStatus.BAD_REQUEST);
-		}
-
-		Usuario usuario = new Usuario(nuevoUsuario.getNombre(), nuevoUsuario.getNombreUsuario(),
-				nuevoUsuario.getEmail(), passwordEncoder.encode(nuevoUsuario.getPassword()));
-
-		Set<Rol> roles = new HashSet<>();
-		roles.add(rolService.getByRolNombre(RolNombre.ROLE_USER).orElseThrow());
-		if(nuevoUsuario.getRoles().contains("admin"))
-			roles.add(rolService.getByRolNombre(RolNombre.ROLE_ADMIN).get());
-		usuario.setRoles(roles);
-		usuarioService.save(usuario);
+	public ResponseEntity<?> nuevoUsuario(@Valid @RequestBody NuevoUsuario nuevoUsuario) {
+		usuarioService.saveUser(nuevoUsuario);
 		return new ResponseEntity<>(new Mensaje("Usuario creado"), HttpStatus.CREATED);
 	}
+
+
 	@PostMapping("/login")
-	public ResponseEntity<JwtDto> login(@Valid @RequestBody LoginUsuario loginUsuario, BindingResult bindingResult){
-		if (bindingResult.hasErrors())
-			return new ResponseEntity(new Mensaje("Campos mal"), HttpStatus.BAD_REQUEST);
-		Authentication authentication =
-				authenticationManager.authenticate(
-						new UsernamePasswordAuthenticationToken(loginUsuario.getNombreUsuario(),
-								loginUsuario.getPassword()));
-		SecurityContextHolder.getContext().setAuthentication(authentication);
-		String jwt = jwtProvider.generateToken(authentication);
-		UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-		JwtDto jwtDto = new JwtDto(jwt, userDetails.getUsername(), userDetails.getAuthorities());
-		return new ResponseEntity<>(jwtDto, HttpStatus.OK);
+	public ResponseEntity<JwtDto> login(@Valid @RequestBody LoginUsuario loginUsuario ){
+	JwtDto jwtDto= usuarioService.login(loginUsuario);
+		return new ResponseEntity(jwtDto, HttpStatus.OK);
 	}
 }
