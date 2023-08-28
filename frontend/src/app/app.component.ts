14bb4b51664e5ceac43f9e5a5e4bf9e7743b6935
Federico <federicoburgos.dev@gmail.com>
2023-08-28 15:50:28 -0300

    template de la pagina principal

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { faHouse, faDollarSign, faArrowRightFromBracket, faGear } from '@fortawesome/free-solid-svg-icons';
+import { faBell } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   title = 'uala';
+
+  userLogged: boolean = true;
+
+  faHouse = faHouse;
+  faDollarSign = faDollarSign;
+  faGear = faGear;
+  faArrowRightFromBracket = faArrowRightFromBracket;
+  faBell = faBell;
+
+  user = {
+    name: 'Federico',
+    lastname: 'Burgos',
+    balance: 365400
+  }
+
 }
