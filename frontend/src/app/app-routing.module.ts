69f559ddbc45229db48734d66e150b8876b5cea5
Federico <federicoburgos.dev@gmail.com>
2023-08-28 15:48:47 -0300

    añadida ruta para home

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,9 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
   },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.component').then(c => c.HomeComponent),
+    title: 'Ualá'
+  }
 ];
 
 @NgModule({
