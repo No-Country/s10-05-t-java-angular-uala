e96e48b80673e6803f3b0c80005fa665c6c9ca32
unknown <saavedra.pedro@yahoo.com.ar>
2023-08-29 09:55:12 -0300

    login

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  
+  {
+  path: 'login',
+  component: LoginComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  
 })
 export class AppRoutingModule { }
