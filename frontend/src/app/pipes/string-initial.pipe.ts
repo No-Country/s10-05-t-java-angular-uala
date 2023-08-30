2d18fe6467a5c5e64e929a4924b55ebeb38b9484
Federico <federicoburgos.dev@gmail.com>
2023-08-30 10:48:20 -0300

    string-initial pipe

---
+++ frontend/src/app/pipes/string-initial.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'stringInitial'
+})
+export class StringInitialPipe implements PipeTransform {
+
+  transform(value: string, ...args: unknown[]): unknown {
+    return value.slice(0, 1);
+  }
+
+}
