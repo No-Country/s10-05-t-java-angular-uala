51641223a36b3a74910a500948dbd39a4d124888
Marcos Paz Goncheff <69874820+marcosep192000@users.noreply.github.com>
2023-08-30 11:03:30 -0300

    :fire: refactor:wallet

---
+++ backend/uala/src/main/java/com/noCountry/uala/models/dto/response/WalletResponseDto.java
@@ -0,0 +1,9 @@
+package com.noCountry.uala.models.dto.response;
+
+import com.noCountry.uala.enums.PaymentMethods;
+
+public class WalletResponseDto {
+
+	private PaymentMethods paymentMethods;
+	private double cashAmount;
+}
