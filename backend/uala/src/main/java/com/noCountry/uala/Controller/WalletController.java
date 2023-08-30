61d5f6fefc1a99d1bb75dc0309b058d68d6dce09
Marcos Paz Goncheff <69874820+marcosep192000@users.noreply.github.com>
2023-08-30 10:59:58 -0300

    :fire: refactor: wallet

--- backend/uala/src/main/java/com/noCountry/uala/Controller/WalletController.java
+++ backend/uala/src/main/java/com/noCountry/uala/Controller/WalletController.java
@@ -1,4 +1,18 @@
 package com.noCountry.uala.Controller;
 
+import com.noCountry.uala.service.PaymentsService.impl.WallerServiceIpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/api/wallet")
 public class WalletController {
+	private final WallerServiceIpl wallerServiceIpl;
+	@GetMapping("/Payments-List")
+	public ResponseEntity<?> ListPayments(){
+		return new ResponseEntity<>(wallerServiceIpl.paymentsList(), HttpStatus.ACCEPTED);
+	}
 }
--- backend/uala/src/main/java/com/noCountry/uala/models/entity/Wallet.java
+++ backend/uala/src/main/java/com/noCountry/uala/models/entity/Wallet.java
@@ -1,6 +1,7 @@
 package com.noCountry.uala.models.entity;
 
-import com.noCountry.uala.security.entity.Usuario;
+import com.noCountry.uala.models.entity.payamentsMethod.Payments;
+
 import lombok.*;
 
 import javax.persistence.*;
@@ -15,35 +16,33 @@ import java.util.List;
 @Entity
 @Table(name = "Wallet")
 public class Wallet {
+
 	@Id
 	@Column(name = "id", nullable = false)
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private double balance;
 	private long cbu;
+	@OneToMany(mappedBy = "wallet",cascade = CascadeType.ALL,orphanRemoval =true)
+	private  List<Payments> paymentsList = new ArrayList<>();
+
 
 	public Wallet(double balance, long cbu ) {
 		this.balance = balance ;
 		this.cbu= cbu;
 	}
-
 	public double generatedCbu(){
 		long cbu  = (long) (Math.random()* 5000000 * 30000);
 		return cbu;
 	}
-
-
-
-	//private List<?> listDeposit = new ArrayList<>();
-
-
-
-
-
-
-
-	public void addDeposit(){
-	//	this.listDeposit.add(null);
-		//completar cuando agreguemos Depositos
-	};
+	 public  void listPayment(Payments payments){
+		paymentsList.add(payments);
+	}
+	public  double totalList(){
+		double cont=0;
+		for (Payments payments: paymentsList) {
+			cont = cont + payments.getCashAmount();
+		}
+		return cont;
+	}
 }
