0a1f68399390c220023f2454fe75f06a17b30b8e
Marcos Paz Goncheff <69874820+marcosep192000@users.noreply.github.com>
2023-08-30 19:31:44 -0300

    :fire: refactor: AuthController and CreateRoles

--- backend/uala/src/main/java/com/noCountry/uala/security/controller/AuthController.java
+++ backend/uala/src/main/java/com/noCountry/uala/security/controller/AuthController.java
@@ -3,8 +3,11 @@ package com.noCountry.uala.security.controller;
 import com.noCountry.uala.security.dto.JwtDto;
 import com.noCountry.uala.security.dto.LoginUsuario;
 import com.noCountry.uala.security.dto.NuevoUsuario;
+import com.noCountry.uala.security.entity.Usuario;
 import com.noCountry.uala.security.service.UsuarioService;
+import com.noCountry.uala.security.util.GetUserLogged;
 import com.noCountry.uala.security.util.Mensaje;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,21 +16,23 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/v1/api/auth")
 public class AuthController {
-
-	@Autowired
-	private UsuarioService usuarioService;
-
+	private final UsuarioService usuarioService;
 	@PostMapping("/add")
 	public ResponseEntity<?> nuevoUsuario(@Valid @RequestBody NuevoUsuario nuevoUsuario) {
 		usuarioService.saveUser(nuevoUsuario);
 			return new ResponseEntity<>(new Mensaje("Created User"), HttpStatus.CREATED);
 	}
-
 	@PostMapping("/login")
 	public ResponseEntity<JwtDto> login(@Valid @RequestBody LoginUsuario loginUsuario ){
 	JwtDto jwtDto= usuarioService.login(loginUsuario);
 		return new ResponseEntity(jwtDto, HttpStatus.OK);
+	}
+	@GetMapping("/infoUser")
+	public ResponseEntity<?> infoUser(){
+		return new ResponseEntity<>(
+				usuarioService.userOfSession(),HttpStatus.OK);
 	}
 }
