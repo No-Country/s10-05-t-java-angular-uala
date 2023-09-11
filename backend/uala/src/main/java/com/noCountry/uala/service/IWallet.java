b64ff191b113fb74406ad8f7a49ca859c05c885d
Marcos Paz Goncheff <69874820+marcosep192000@users.noreply.github.com>
2023-09-10 23:53:50 -0300

    :zap: refactor : all modified interfaces

--- backend/uala/src/main/java/com/noCountry/uala/service/IWallet.java
+++ backend/uala/src/main/java/com/noCountry/uala/service/IWallet.java
@@ -6,5 +6,6 @@ import java.util.List;
 
 public interface IWallet {
 	List<Payments> paymentsList();
+	void addCashWallet(double cash);
 
 }
---
+++ backend/uala/src/main/java/com/noCountry/uala/service/PaymentsService/ITransferToThirdParties.java
@@ -0,0 +1,18 @@
+package com.noCountry.uala.service.PaymentsService;
+
+import com.noCountry.uala.models.dto.request.TranferMethodDto;
+import com.noCountry.uala.models.dto.request.UserCbuOrAliasRequestDto;
+import com.noCountry.uala.models.entity.Wallet;
+import com.noCountry.uala.security.dto.UserResponseDto;
+import com.noCountry.uala.security.entity.Usuario;
+
+public interface ITransferToThirdParties {
+
+	public abstract boolean sendTransfer(TranferMethodDto tranferMethodDto);
+	public abstract UserResponseDto  findToCbuOrAlias(UserCbuOrAliasRequestDto dato);
+
+
+
+
+
+}
