fe2e931f3f9d7b83728ab16bb6c06d3d1f9a80e3
Federico <federicoburgos.dev@gmail.com>
2023-09-11 06:31:45 -0300

    home component recibe userInfo

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { BankingComponent } from './components/banking/banking.component';
 import { MovementsComponent } from './components/movements/movements.component';
 import { ExpensesComponent } from './components/expenses/expenses.component';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -17,40 +18,47 @@ import { ExpensesComponent } from './components/expenses/expenses.component';
 })
 export class HomeComponent {
 
-  user = {
-    name: 'Federico',
-    lastname: 'Burgos',
-    balance: 365400,
-    expenses: [
-      {
-        name: 'Servicios y débitos automáticos',
-        value: 7000
-      },
-      {
-        name: 'Compras',
-        value: 5000
-      },
-      {
-        name: 'Otros',
-        value: 3000
-      },
-      {
-        name: 'Supermercados y alimentos',
-        value: 1900
-      },
-      {
-        name: 'Restaurantes y bares',
-        value: 1500
-      },
-      {
-        name: 'Salud y deportes',
-        value: 1200
-      },
-      {
-        name: 'Transporte y auto',
-        value: 1200
+  authService = inject(AuthService);
+
+  userInfo: any;
+
+  ngOnInit(): void {
+    this.authService.getUserInfoObservable().subscribe({
+      next: (data) => {
+        this.userInfo = data;
       }
-    ]
-  }
+    });
+  }  
+
+  expenses = [
+    {
+      name: 'Servicios y débitos automáticos',
+      value: 7000
+    },
+    {
+      name: 'Compras',
+      value: 5000
+    },
+    {
+      name: 'Otros',
+      value: 3000
+    },
+    {
+      name: 'Supermercados y alimentos',
+      value: 1900
+    },
+    {
+      name: 'Restaurantes y bares',
+      value: 1500
+    },
+    {
+      name: 'Salud y deportes',
+      value: 1200
+    },
+    {
+      name: 'Transporte y auto',
+      value: 1200
+    }
+  ];
 
 }
