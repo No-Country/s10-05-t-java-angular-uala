adc38eb9fb549cec9e561f34436abdc0ec684572
Federico <federicoburgos.dev@gmail.com>
2023-09-11 06:27:34 -0300

    creada logica de userInfo en authService

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,14 +1,35 @@
 import { Injectable, inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  http = inject(HttpClient)
+  http = inject(HttpClient);
+
+  userInfo: BehaviorSubject<any> = new BehaviorSubject(undefined);
 
   login(credentials: any) {
     return this.http.post('https://uala-no-country.onrender.com/v1/api/auth/login', credentials);
   }
+
+  getUserInfo() {
+    let token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+    const options = { headers: headers };
+    this.http.get('https://uala-no-country.onrender.com/v1/api/auth/infoUser', options).subscribe({
+      next: (res) => {
+        this.userInfo.next(res);
+      }
+    });
+  }
+
+  getUserInfoObservable() {
+    return this.userInfo.asObservable();
+  }
+
 }
