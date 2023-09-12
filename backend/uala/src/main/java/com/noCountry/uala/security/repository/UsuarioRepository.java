a94576325e9a44cfde6143162bb3be6844bcab97
Marcos Paz Goncheff <69874820+marcosep192000@users.noreply.github.com>
2023-09-12 01:41:42 -0300

    :bug: refactor : Interfaces

--- backend/uala/src/main/java/com/noCountry/uala/security/repository/UsuarioRepository.java
+++ backend/uala/src/main/java/com/noCountry/uala/security/repository/UsuarioRepository.java
@@ -3,9 +3,12 @@ package com.noCountry.uala.security.repository;
 import com.noCountry.uala.security.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+	List<Usuario> findByContactos(Usuario contactos);
+	Usuario findByContactos_IdUsuario(int idUsuario);
 	Usuario findByNombre(String nombre);
 	Optional<Usuario> findByNombreUsuario(String nombreUsuario);
 	boolean existsByNombreUsuario (String nombreUsuario);
