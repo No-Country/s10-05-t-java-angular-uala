0ebc4dca7b862c9fe8d648aefdf116e4addd3b74
Federico <federicoburgos.dev@gmail.com>
2023-09-12 04:37:22 -0300

    creada página work in progress

---
+++ frontend/src/app/pages/work-in-progress/work-in-progress.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-work-in-progress',
+  templateUrl: './work-in-progress.component.html',
+  styleUrls: ['./work-in-progress.component.css'],
+  standalone: true,
+  imports: [
+    RouterModule
+  ]
+})
+export class WorkInProgressComponent {
+
+}
