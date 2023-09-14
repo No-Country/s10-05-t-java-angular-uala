945a2ee6c14478d325693b2c6f2f6969707c1177
Federico <federicoburgos.dev@gmail.com>
2023-09-14 00:37:57 -0300

    responsive y logica de transferencias

--- frontend/src/app/services/trade.service.ts
+++ frontend/src/app/services/trade.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
@@ -6,7 +7,10 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class TradeService {
 
+  http = inject(HttpClient);
+
   formStep: BehaviorSubject<number> = new BehaviorSubject(0);
+  transferDestiny: BehaviorSubject<any> = new BehaviorSubject(undefined);
 
   setFormStep(value: number) {
     this.formStep.next(value);
@@ -16,8 +20,39 @@ export class TradeService {
     return this.formStep.asObservable();
   }
 
-  submitTrade(trade: any) {
-    console.log(trade);
+  setTransferDestiny(value: any) {
+    this.transferDestiny.next(value);
+  }
+
+  getTransferDestiny() {
+    return this.transferDestiny.asObservable();
+  }
+
+  submitTrade(data: any) {
+    let token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+    const options = { headers: headers };
+    return this.http.post('https://uala-no-country.onrender.com/v1/api/transfer/send', data, options);
+  }
+
+  getUserContacts() {
+    let token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+    const options = { headers: headers };
+    return this.http.get('https://uala-no-country.onrender.com/v1/api/wallet/contact', options);
+  }
+
+  findUser(alias: string) {
+    let token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+    const options = { headers: headers };
+    return this.http.get(`https://uala-no-country.onrender.com/v1/api/transfer/serch-cbu-alias/${alias}`, options);
   }
 
 }
